604cc4a462a8dfae8be0d096264e553c10f7c450
GuiAlvesdev <guialves1353@gmail.com>
2023-01-16 22:11:49 -0300

    adicionado primeiras receitas a lista como exemplo :tada:

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[] = [
+    new Recipe('Lasagna', 'This is simply test', 'https://img.itdg.com.br/tdg/images/recipes/000/000/876/324587/324587_original.jpg?mode=crop&width=710&height=400'),
+    new Recipe('Lasagna', 'This is simply test', 'https://img.itdg.com.br/tdg/images/recipes/000/000/876/324587/324587_original.jpg?mode=crop&width=710&height=400')
+  ];
+
+  constructor(){
+
+  }
+
+
+  ngOnInit(): void {
+
+  }
+
 
 }
